2ae8449af64bc2811ed4dacfd48278ba0f5d018d
Panin Aleksandr <Aleksandr.Panin@burda-forward.de>
2024-05-28 17:43:23 +0200

    checking pipeline

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> std::io::Result<()> {
     let connection_pool =
         PgPool::connect(&configuration.database.connection_string())
             .await
-            .expect("Failed to connect to Postgres");
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
