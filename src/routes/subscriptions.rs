5d08dc2da97d85a56cfc7d8705bab0ca6bfe5091
Your Name <you@example.com>
2024-06-18 18:30:39 +0200

    validation for name

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,3 +1,4 @@
+use crate::domain::{NewSubscriber, SubscriberName};
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::SqlitePool;
@@ -16,8 +17,20 @@ pub struct FormData {
         subscriber_name= %form.name
     )
 )]
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<SqlitePool>) -> impl Responder {
-    match insert_subscriber(&form, &pool).await {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<SqlitePool>,
+) -> impl Responder {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let new_subscriber = NewSubscriber {
+        email: form.0.email,
+        name,
+    };
+
+    match insert_subscriber(&new_subscriber, &pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
@@ -25,17 +38,21 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<SqlitePool>) -
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
-pub async fn insert_subscriber(form: &FormData, pool: &SqlitePool) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(
+    new_subscriber: &NewSubscriber,
+    pool: &SqlitePool,
+) -> Result<(), sqlx::Error> {
     let now = Utc::now();
+    let name = new_subscriber.name.as_ref();
     sqlx::query!(
         r#"
             INSERT INTO subscriptions (email, name, subscribed_at) 
             VALUES ($1, $2, $3)
         "#,
-        form.email,
-        form.name,
+        new_subscriber.email,
+        name,
         now
     )
     .execute(pool)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -41,7 +41,8 @@ async fn spawn_app() -> TestApp {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, connection_pool.clone()).expect("Failed to connect to Sqlite");
+    let server =
+        run(listener, connection_pool.clone()).expect("Failed to connect to Sqlite");
     let _ = tokio::spawn(server);
     TestApp {
         address,
@@ -105,6 +106,32 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             response.status().as_u16(),
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
+        );
+    }
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
         );
     }
 }
