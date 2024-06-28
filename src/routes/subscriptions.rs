9bb3501fec5d9f0b58a0f72a35074fccbec2270f
Your Name <you@example.com>
2024-06-28 20:41:47 +0200

    refactor errors

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,9 @@
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::http::StatusCode;
+use actix_web::{web, HttpResponse, ResponseError};
 use chrono::Utc;
 use sqlx::SqlitePool;
+use sqlx::{Sqlite, Transaction};
 use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
@@ -31,43 +33,77 @@ impl TryFrom<FormData> for NewSubscriber {
 pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<SqlitePool>,
-) -> impl Responder {
-    let new_subscriber = match form.0.try_into() {
-        Ok(subscriber) => subscriber,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    match insert_subscriber(&new_subscriber, &pool).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
+) -> Result<HttpResponse, SubscribeError> {
+    let new_subscriber = form.0.try_into()?;
+    let mut transaction = pool.begin().await?;
+    insert_subscriber(&new_subscriber, &mut transaction).await?;
+    // if insert_subscriber(&new_subscriber, &mut transaction).await.is_err() {
+    //     return HttpResponse::InternalServerError().finish();
+    // }
+    transaction.commit().await?;
+    Ok(HttpResponse::Ok().finish())
 }
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(new_subscriber, pool)
+    skip(new_subscriber, transaction)
 )]
 pub async fn insert_subscriber(
     new_subscriber: &NewSubscriber,
-    pool: &SqlitePool,
+    transaction: &mut Transaction<'_, Sqlite>,
 ) -> Result<(), sqlx::Error> {
     let now = Utc::now();
     let name = new_subscriber.name.as_ref();
     let email = new_subscriber.email.as_ref();
     sqlx::query!(
         r#"
-            INSERT INTO subscriptions (email, name, subscribed_at) 
-            VALUES ($1, $2, $3)
+            INSERT INTO subscriptions (email, name, subscribed_at, status) 
+            VALUES ($1, $2, $3, 'pending_confirmation')
         "#,
         email,
         name,
         now
     )
-    .execute(pool)
+    .execute(transaction.as_mut())
     .await
     .map_err(|e| {
         tracing::error!("Failed to execute query: {:?}", e);
         e
     })?;
     Ok(())
+}
+
+#[derive(Debug)]
+pub enum SubscribeError {
+    ValidationError(String),
+    DatabaseError(sqlx::Error),
+}
+
+impl std::error::Error for SubscribeError {}
+
+impl ResponseError for SubscribeError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
+            SubscribeError::DatabaseError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+impl From<sqlx::Error> for SubscribeError {
+    fn from(e: sqlx::Error) -> Self {
+        Self::DatabaseError(e)
+    }
+}
+
+impl From<String> for SubscribeError {
+    fn from(e: String) -> Self {
+        Self::ValidationError(e)
+    }
+}
+
+impl std::fmt::Display for SubscribeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Failed to create a new subscriber.")
+    }
 }
